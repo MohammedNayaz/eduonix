94e41742aa51b917baae049a2cdfaed8925fcb98
Mohammed Nayaz <mohammednayaz493@gmail.com>
2021-06-12 17:36:23 +0530

    first commit

---
+++ checkout/app/Http/Controllers/ItemController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Item;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items = Item::orderBy('created_at', 'desc')->get();
+
+        return response()->json($items);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $item = new Item();
+        $item->body = $request->input('body');
+        $item->save();
+
+        return response()->json($item);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $item = Item::find($id);
+
+        if ($item == null) {
+            return response()->json('Item not found', 422);
+
+        } else {
+            $item->is_checked = !$item->is_checked;
+            $item->save();
+            return response()->json($item);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $item = Item::find($id);
+
+        if ($item == null) {
+            return response()->json('Item not found', 422);
+
+        } else {
+            $item->delete();
+            return response()->json();
+        }
+    }
+}
